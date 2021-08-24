6f051f7cf0822accbcdea67d13d9fed815ffc79c
Rogger <bogotarogger@gmail.com>
2021-08-23 19:29:32 -0500

    proyecto base

--- Frontend/todo-app/src/app/app.component.ts
+++ Frontend/todo-app/src/app/app.component.ts
@@ -8,19 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'todo-app';
 
-  nombre: string = "Samuel";
-  nombreGuardado: string = "";
-  casado: boolean = false;
-  edad: number = 17;
-  listaNombres: string[] = ["Alejandro","Cristian","Rogger","Sergio"];
-  persona = {
-    name: "Rogger",
-    age: 24,
-    married: false
-  }
-
-  guardar(){
-    this.nombreGuardado = this.nombre;
-    this.listaNombres.push(this.nombre);
-  }
+  
 }
